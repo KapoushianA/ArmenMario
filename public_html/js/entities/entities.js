7e4a12be063e6b800e783e48f94a0a934eb52555
KapoushianA <armen.goml@yahoo.com>
2014-12-09 11:29:11 -0800

    done with 23

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -13,10 +13,13 @@ game.PlayerEntity = me.Entity.extend({
         }]);
         
         this.renderable.addAnimation("idle", [3]);
+        this.renderable.addAnimation("bigIdle", [19]);
         this.renderable.addAnimation("smallWalk", [ 8, 9, 10, 11, 12, 13], 80);
+        this.renderable.addAnimation("bigWalk", [14, 15, 16, 17, 18, 19], 80);
         
         this.renderable.setCurrentAnimation("idle");
         
+        this.big = false;
         this.body.setVelocity(5, 20);
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
@@ -45,7 +48,7 @@ game.PlayerEntity = me.Entity.extend({
         
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
-        
+        if(!this.big){
         if(this.body.vel.x !==0){
           if(!this.renderable.isCurrentAnimation("smallWalk")){ 
               this.renderable.setCurrentAnimation("smallWalk");
@@ -54,6 +57,20 @@ game.PlayerEntity = me.Entity.extend({
         }else{
             this.renderable.setCurrentAnimation("idle");
         }
+    }else{    
+        this.body.update(delta);
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
+        if(!this.big){
+        if(this.body.vel.x !==0){
+          if(!this.renderable.isCurrentAnimation("smallWalk")){ 
+              this.renderable.setCurrentAnimation("smallWalk");
+              this.renderable.setAnimationFrame();
+              }
+        }else{
+            this.renderable.setCurrentAnimation("idle");
+        }
+        
+    }
         
         this.body.update(delta);
         this._super(me.Entity, "update", [delta]);
@@ -72,8 +89,11 @@ game.PlayerEntity = me.Entity.extend({
             }
              me.state.change(me.state.MENU);
         }
+    }else if(response.b.type === 'mushroom'){
+        this.big = true;
+         me.game.world.removeChild(response.b);
     }
-    
+   }    
     });
       
     game.LevelTrigger = me.Entity.extend({
@@ -152,3 +172,22 @@ game.BadGuy = me.Entity.extend({
          
     
 });
+
+game.Mushroom = me.Entity.extend({
+        init: function(x, y, settings){
+        this._super(me.Entity, 'init', [x, y, {
+             image: "mushroom",
+             spritewidth: "64",
+             spriteheight: "64",
+             width: 64,
+             height: 64,
+             getShape: function(){
+                 return (new me.Rect(0, 0, 64, 64)).toPolygon();
+             }
+        }]);
+    
+        me.collision.check(this);
+        this.type = "mushroom";
+    
+      }
+   });
